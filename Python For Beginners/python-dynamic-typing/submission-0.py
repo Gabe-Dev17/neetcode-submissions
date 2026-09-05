e58b9635fdc72c4055b38a18e6dfa6b6b959def6
Gabriel A. Rivera <66492149+Gabe-Dev17@users.noreply.github.com>
2026-09-05 16:57:15 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hey"
+print(type(variable))
+variable = [1,0]
+print(type(variable))

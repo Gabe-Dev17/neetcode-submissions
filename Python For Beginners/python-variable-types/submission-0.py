e42f43ef866a9f56cdce7b05715e17ee2c8e5a95
Gabriel A. Rivera <66492149+Gabe-Dev17@users.noreply.github.com>
2026-09-05 16:54:46 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,15 @@
+sum = 10
+float_type = 10.3
+bool_type = True
+string_type = "hey"
+list_type = [1,2,3]
+
+
+
+
+
+print(type(sum))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
